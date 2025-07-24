66a30b1c4f9f7716159d41c993819467e99a6500
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-07-24 09:45:05 -0300

    feat: adiciona botões de salvar, editar e limpar no cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
